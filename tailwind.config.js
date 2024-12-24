8fe1d52ebb4ad2f7acf27d0e5e20f2ae4db00b96
7twik <sattwik.das.ece25@heritageit.edu.in>
2024-12-24 17:59:50 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,9 @@
 export default {
     darkMode: ["class"],
 	content: [
-		"./pages/**/*.{js,ts,jsx,tsx}",
-		"./components/**/*.{js,ts,jsx,tsx}",
-		"./layouts/**/*.{js,ts,jsx,tsx}",   ],
+		'./src/**/*.{js,jsx}',
+		"./src/pages/**/*.{js,jsx}",
+	  ],
   theme: {
   	extend: {
   		screens: {
